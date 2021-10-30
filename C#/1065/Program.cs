24624ae1fa5a8003e8ae6db5917764c8480e7a46
Vitor Barbieri da Silva <vbarbierisilva@gmail.com>
2021-10-30 16:20:01 -0300

    Enviar exercício 1066
    1066 - Pares, Ímpares, Positivos e Negativos

--- C#/1065/Program.cs
+++ C#/1065/Program.cs
@@ -4,33 +4,105 @@ public class Program
 {
     public static void Main()
     {
-        int n1 = int.Parse(Console.ReadLine());
-        int n2 = int.Parse(Console.ReadLine());
-        int n3 = int.Parse(Console.ReadLine());
-        int n4 = int.Parse(Console.ReadLine());
-        int n5 = int.Parse(Console.ReadLine());
+        int pares = 0;
+        int impares = 0;
+        int positivos = 0;
+        int negativos = 0;
 
-        int valoresPares = 0;
-        if (n1 % 2 == 0) 
+
+        int numero = int.Parse(Console.ReadLine());
+        if (numero > 0)
         {
-            valoresPares += 1;
+            positivos += 1;
         }
-        if (n2 % 2 == 0) 
+        else if (numero < 0)
         {
-            valoresPares += 1;
+            negativos += 1;
         }
-        if (n3 % 2 == 0) 
+        if (numero % 2 == 0)
         {
-            valoresPares += 1;
+            pares += 1;
         }
-        if (n4 % 2 == 0) 
+        else
         {
-            valoresPares += 1;
+            impares += 1;
         }
-        if (n5 % 2 == 0) 
+
+        numero = int.Parse(Console.ReadLine());
+        if (numero > 0)
         {
-            valoresPares += 1;
+            positivos += 1;
         }
-        Console.WriteLine(valoresPares + " valores pares");
+        else if (numero < 0)
+        {
+            negativos += 1;
+        }
+        if (numero % 2 == 0)
+        {
+            pares += 1;
+        }
+        else
+        {
+            impares += 1;
+        }
+        
+        numero = int.Parse(Console.ReadLine());
+        if (numero > 0)
+        {
+            positivos += 1;
+        }
+        else if (numero < 0)
+        {
+            negativos += 1;
+        }
+        if (numero % 2 == 0)
+        {
+            pares += 1;
+        }
+        else
+        {
+            impares += 1;
+        }
+        
+        numero = int.Parse(Console.ReadLine());
+        if (numero > 0)
+        {
+            positivos += 1;
+        }
+        else if (numero < 0)
+        {
+            negativos += 1;
+        }
+        if (numero % 2 == 0)
+        {
+            pares += 1;
+        }
+        else
+        {
+            impares += 1;
+        }
+        
+        numero = int.Parse(Console.ReadLine());
+        if (numero > 0)
+        {
+            positivos += 1;
+        }
+        else if (numero < 0)
+        {
+            negativos += 1;
+        }
+        if (numero % 2 == 0)
+        {
+            pares += 1;
+        }
+        else
+        {
+            impares += 1;
+        }
+
+        Console.WriteLine(pares + " valor(es) par(es)");
+        Console.WriteLine(impares + " valor(es) impar(es)");
+        Console.WriteLine(positivos + " valor(es) positivo(s)");
+        Console.WriteLine(negativos + " valor(es) negativo(s)");
     }
 }
