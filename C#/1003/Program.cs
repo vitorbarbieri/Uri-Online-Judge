87e26d48064efd2b57ee8dafee556b500e1f6155
Vitor Barbieri da Silva <vbarbierisilva@gmail.com>
2021-10-30 15:18:32 -0300

    Enviar exercício 1003
    1003 - Soma Simples

---
+++ C#/1003/Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        int A = int.Parse(Console.ReadLine());
+        int B = int.Parse(Console.ReadLine());
+
+        int SOMA = A + B;
+
+        Console.WriteLine("SOMA = " + SOMA);
+    }
+}
