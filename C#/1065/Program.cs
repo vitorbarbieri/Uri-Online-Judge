9588a33f219f64195b85c13ab49e2b5a1622a7dd
Vitor Barbieri da Silva <vbarbierisilva@gmail.com>
2021-10-30 16:13:32 -0300

    Enviar exercício 1065
    1065 - Pares entre Cinco Números

---
+++ C#/1065/Program.cs
@@ -0,0 +1,36 @@
+using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        int n1 = int.Parse(Console.ReadLine());
+        int n2 = int.Parse(Console.ReadLine());
+        int n3 = int.Parse(Console.ReadLine());
+        int n4 = int.Parse(Console.ReadLine());
+        int n5 = int.Parse(Console.ReadLine());
+
+        int valoresPares = 0;
+        if (n1 % 2 == 0) 
+        {
+            valoresPares += 1;
+        }
+        if (n2 % 2 == 0) 
+        {
+            valoresPares += 1;
+        }
+        if (n3 % 2 == 0) 
+        {
+            valoresPares += 1;
+        }
+        if (n4 % 2 == 0) 
+        {
+            valoresPares += 1;
+        }
+        if (n5 % 2 == 0) 
+        {
+            valoresPares += 1;
+        }
+        Console.WriteLine(valoresPares + " valores pares");
+    }
+}
