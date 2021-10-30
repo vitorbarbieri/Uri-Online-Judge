08b2492798e54d6553327b2ca7b1ca7b360695db
Vitor Barbieri da Silva <vbarbierisilva@gmail.com>
2021-10-30 14:35:40 -0300

    Enviar exercício 1001
    1001 - Extremamente Básico

---
+++ C#/1001/Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+
+        int x = a + b;
+
+        Console.WriteLine("X = " + x);
+    }
+}
