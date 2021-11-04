c8ee99c2eed0bbaf9c5858e2e0523fae9bb0f023
Vitor Barbieri da Silva <vbarbierisilva@gmail.com>
2021-11-04 17:48:22 -0300

    Enviar exercício 1021
    1021 - Notas e Moedas

--- C#/1025/Program.cs
+++ C#/1025/Program.cs
@@ -4,6 +4,25 @@ public class Program
 {
     public static void Main()
     {
-        Console.WriteLine("Hello World!");        
+        double total = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        double[] valor = { 100, 50, 20, 10, 5, 2, 1, 0.5, 0.25, 0.1, 0.05, 0.01 };
+        int quantidade;
+
+        Console.WriteLine("NOTAS:");
+        for (int i = 0; i < 6; i++)
+        {
+            quantidade = (int)(total / valor[i]);
+            Console.WriteLine(quantidade + " nota(s) de R$ " + valor[i].ToString("F2", CultureInfo.InvariantCulture));
+            total -= quantidade * valor[i];
+            total = Math.Round(total, 2);
+        }
+        Console.WriteLine("MOEDAS:");
+        for (int i = 6; i < valor.Length; i++)
+        {
+            quantidade = (int)(total / valor[i]);
+            Console.WriteLine(quantidade + " moeda(s) de R$ " + valor[i].ToString("F2", CultureInfo.InvariantCulture));
+            total -= quantidade * valor[i];
+            total = Math.Round(total, 2);
+        }
     }
 }
