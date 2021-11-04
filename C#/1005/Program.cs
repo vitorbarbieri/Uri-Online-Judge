5f043b061216abe6c1c5e6ea2fafef92f02e98ea
Vitor Barbieri da Silva <vbarbierisilva@gmail.com>
2021-11-04 17:34:48 -0300

    Enviar exercício 1005
    1005 - Média 1

--- C#/1005/Program.cs
+++ C#/1005/Program.cs
@@ -4,6 +4,13 @@ public class Program
 {
     public static void Main()
     {
-        Console.WriteLine("Hello World!");        
+        double A, B, MEDIA;
+
+        A = double.Parse(Console.ReadLine());
+        B = double.Parse(Console.ReadLine());
+
+        MEDIA = (A * 3.5 + 7.5 * B) / 11;
+
+        Console.WriteLine("MEDIA = " + MEDIA.ToString("F5"));
     }
 }
