9983c9e75869811d9e8218bfc6edecec14be8efe
Vitor Barbieri da Silva <vbarbierisilva@gmail.com>
2021-11-04 17:53:17 -0300

    Enviar exercício 1035
    1035 - Teste de Seleção 1

--- C#/1035/Program.cs
+++ C#/1035/Program.cs
@@ -4,6 +4,21 @@ public class Program
 {
     public static void Main()
     {
-        Console.WriteLine("Hello World!");        
+        string texto = Console.ReadLine();
+        string[] vet = texto.Split(' ');
+
+        int a = int.Parse(vet[0]);
+        int b = int.Parse(vet[1]);
+        int c = int.Parse(vet[2]);
+        int d = int.Parse(vet[3]);
+
+        if (b > c && d > a && c + d > a + b && c > 0 && d > 0 && a % 2 == 0)
+        {
+            Console.WriteLine("Valores aceitos");
+        }
+        else
+        {
+            Console.WriteLine("Valores nao aceitos");
+        }
     }
 }
