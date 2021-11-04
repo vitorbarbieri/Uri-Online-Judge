d904035f5a28a3a3115c90c926fc02e754b459fa
Vitor Barbieri da Silva <vbarbierisilva@gmail.com>
2021-11-04 17:42:01 -0300

    Enviar exercício 1013
    1013 - O Maior

--- C#/1013/Program.cs
+++ C#/1013/Program.cs
@@ -4,6 +4,19 @@ public class Program
 {
     public static void Main()
     {
-        Console.WriteLine("Hello World!");        
+        string str;
+        int a, b, c, maiorAB, maiorABC;
+
+        str = Console.ReadLine();
+        string[] vet = str.Split(' ');
+
+        a = int.Parse(vet[0]);
+        b = int.Parse(vet[1]);
+        c = int.Parse(vet[2]);
+
+        maiorAB = (a + b + Math.Abs(a - b)) / 2;
+        maiorABC = (maiorAB + c + Math.Abs(maiorAB - c)) / 2;
+
+        Console.WriteLine(maiorABC + " eh o maior");    
     }
 }
