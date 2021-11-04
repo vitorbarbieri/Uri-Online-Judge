c37a9ba9526439ccd20e7df8e7792732b227cb38
Vitor Barbieri da Silva <vbarbierisilva@gmail.com>
2021-11-04 17:41:37 -0300

    Enviar exercício 1012
    1012 - Área

--- C#/1012/Program.cs
+++ C#/1012/Program.cs
@@ -4,6 +4,26 @@ public class Program
 {
     public static void Main()
     {
-        Console.WriteLine("Hello World!");        
+        double a, b, c, aTri, aCir, aTra, aQua, aRet;
+        string texto;
+
+        texto = Console.ReadLine();
+        string[] vet = texto.Split(' ');
+
+        a = double.Parse(vet[0]);
+        b = double.Parse(vet[1]);
+        c = double.Parse(vet[2]);
+
+        aTri = (a * c) / 2.0;
+        aCir = 3.14159 * Math.Pow(c, 2);
+        aTra = ((a + b) * c) / 2.0;
+        aQua = Math.Pow(b, 2);
+        aRet = a * b;
+
+        Console.WriteLine("TRIANGULO: " + aTri.ToString("F3"));
+        Console.WriteLine("CIRCULO: " + aCir.ToString("F3"));
+        Console.WriteLine("TRAPEZIO: " + aTra.ToString("F3"));
+        Console.WriteLine("QUADRADO: " + aQua.ToString("F3"));
+        Console.WriteLine("RETANGULO: " + aRet.ToString("F3"));
     }
 }
