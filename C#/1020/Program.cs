d59757fae95a80ed94bc16bd757398e7e0a13bcd
Vitor Barbieri da Silva <vbarbierisilva@gmail.com>
2021-11-04 17:47:50 -0300

    Enviar exercício 1020
    1020 - Idade em Dias

--- C#/1020/Program.cs
+++ C#/1020/Program.cs
@@ -4,6 +4,17 @@ public class Program
 {
     public static void Main()
     {
-        Console.WriteLine("Hello World!");        
+        int total, ano, mes, dia, resto;
+
+        total = int.Parse(Console.ReadLine());
+
+        ano = total / 365;
+        resto = total % 365;
+        mes = resto / 30;
+        dia = resto % 30;
+
+        Console.WriteLine(ano + " ano(s)");
+        Console.WriteLine(mes + " mes(es)");
+        Console.WriteLine(dia + " dia(s)");
     }
 }
