f9d4674f8c91ecf19a7a8e43b672b310b7e45d1d
Vitor Barbieri da Silva <vbarbierisilva@gmail.com>
2021-11-04 17:45:57 -0300

    Enviar exercício 1017
    1017 - Gasto de Combustível

--- C#/1017/Program.cs
+++ C#/1017/Program.cs
@@ -4,6 +4,15 @@ public class Program
 {
     public static void Main()
     {
-        Console.WriteLine("Hello World!");        
+        int tempo, velocidade;
+        double distancia, qtLitros;
+
+        tempo = int.Parse(Console.ReadLine());
+        velocidade = int.Parse(Console.ReadLine());
+
+        distancia = tempo * velocidade;
+        qtLitros = distancia / 12;
+
+        Console.WriteLine(qtLitros.ToString("F3"));
     }
 }
