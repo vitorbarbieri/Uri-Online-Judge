dece4c9e3897dbb3c997ce13a935fa9c6b3bfc51
Vitor Barbieri da Silva <vbarbierisilva@gmail.com>
2021-11-04 18:19:06 -0300

    Enviar exercício 1059
    1059 - Números Pares

---
+++ C#/1059/Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        int num = 2;
+        while (num <= 100)
+        {
+            Console.WriteLine(num);
+            num += 2;
+        }
+    }
+}
