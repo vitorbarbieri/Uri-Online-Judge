cbb0df8026d39a5e44faebcac55886102eca902f
Vitor Barbieri da Silva <vbarbierisilva@gmail.com>
2021-11-04 17:57:33 -0300

    Enviar exercício 1046
    1046 - Tempo de Jogo

--- C#/1046/Program.cs
+++ C#/1046/Program.cs
@@ -4,6 +4,28 @@ public class Program
 {
     public static void Main()
     {
-        Console.WriteLine("Hello World!");        
+        string texto = Console.ReadLine();
+        string[] vet = texto.Split(' ');
+
+        int a = int.Parse(vet[0]);
+        int b = int.Parse(vet[1]);
+        int duracao;
+
+        if (a == b)
+        {
+            duracao = 24;
+        }
+        else
+        {
+            if (a < b)
+            {
+                duracao = b - a;
+            }
+            else
+            {
+                duracao = 24 - (a - b);
+            }
+        }
+        Console.WriteLine("O JOGO DUROU " + duracao + " HORA(S)");
     }
 }
--- C#/1048/Program.cs
+++ C#/1048/Program.cs
@@ -4,28 +4,6 @@ public class Program
 {
     public static void Main()
     {
-        string texto = Console.ReadLine();
-        string[] vet = texto.Split(' ');
-
-        int a = int.Parse(vet[0]);
-        int b = int.Parse(vet[1]);
-        int duracao;
-
-        if (a == b)
-        {
-            duracao = 24;
-        }
-        else
-        {
-            if (a < b)
-            {
-                duracao = b - a;
-            }
-            else
-            {
-                duracao = 24 - (a - b);
-            }
-        }
-        Console.WriteLine("O JOGO DUROU " + duracao + " HORA(S)");
+        Console.WriteLine("Hello World!");        
     }
 }
