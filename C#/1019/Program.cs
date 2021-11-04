6fc2459deea394cdf84c9878bc64025e07f300ab
Vitor Barbieri da Silva <vbarbierisilva@gmail.com>
2021-11-04 17:47:28 -0300

    Enviar exercício 1019
    1019 - Conversão de Tempo

--- C#/1019/Program.cs
+++ C#/1019/Program.cs
@@ -4,6 +4,16 @@ public class Program
 {
     public static void Main()
     {
-        Console.WriteLine("Hello World!");        
+        int tempo, hora, minuto, segundo, resto1, resto2;
+
+        tempo = int.Parse(Console.ReadLine());
+
+        hora = tempo / 3600;
+        resto1 = tempo % 3600;
+        minuto = resto1 / 60;
+        resto2 = resto1 % 60;
+        segundo = resto2;
+
+        Console.WriteLine(hora + ":" + minuto + ":" + segundo);
     }
 }
