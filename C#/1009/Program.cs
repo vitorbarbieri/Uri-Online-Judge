ed1d14338a8f606bc90fed83d3cb969551564876
Vitor Barbieri da Silva <vbarbierisilva@gmail.com>
2021-11-04 17:38:26 -0300

    Enviar exercício 1009
    1009 - Salário com Bônus

--- C#/1009/Program.cs
+++ C#/1009/Program.cs
@@ -4,6 +4,15 @@ public class Program
 {
     public static void Main()
     {
-        Console.WriteLine("Hello World!");        
+        string nome;
+        double salario, venda, total;
+
+        nome = Console.ReadLine();
+        salario = double.Parse(Console.ReadLine());
+        venda = double.Parse(Console.ReadLine());
+
+        total = salario + venda * 0.15;
+
+        Console.WriteLine("TOTAL = R$ " + total.ToString("F2"));
     }
 }
