1ad370320966ca34efceb85dfea009c2e9557d61
Vitor Barbieri da Silva <vbarbierisilva@gmail.com>
2021-11-04 17:58:02 -0300

    Enviar exercício 1048
    1048 - Aumento de Salário

--- C#/1048/Program.cs
+++ C#/1048/Program.cs
@@ -4,6 +4,51 @@ public class Program
 {
     public static void Main()
     {
-        Console.WriteLine("Hello World!");        
+            double salario = double.Parse(Console.ReadLine());
+            double novoSalario, reajuste;
+            int percentual;
+
+            if (salario <= 400) 
+            {
+                novoSalario = salario * 1.15;
+                reajuste = novoSalario - salario;
+                percentual = 15;
+            }
+            else
+            {
+                if (salario <= 800)
+                {
+                    novoSalario = salario * 1.12;
+                    reajuste = novoSalario - salario;
+                    percentual = 12;
+                }
+                else
+                {
+                    if (salario <= 1200)
+                    {
+                        novoSalario = salario * 1.10;
+                        reajuste = novoSalario - salario;
+                        percentual = 10;
+                    }
+                    else
+                    {
+                        if (salario <= 2000)
+                        {
+                            novoSalario = salario * 1.07;
+                            reajuste = novoSalario - salario;
+                            percentual = 7;
+                        }
+                        else
+                        {
+                            novoSalario = salario * 1.04;
+                            reajuste = novoSalario - salario;
+                            percentual = 4;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("Novo salario: " + novoSalario.ToString("F2"));
+            Console.WriteLine("Reajuste ganho: " + reajuste.ToString("F2"));
+            Console.WriteLine("Em percentual: " + percentual + " %");       
     }
 }
