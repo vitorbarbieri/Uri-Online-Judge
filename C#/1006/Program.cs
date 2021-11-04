00b8458aa440b24b18fc601c55c11a63bbb4fc93
Vitor Barbieri da Silva <vbarbierisilva@gmail.com>
2021-11-04 17:35:30 -0300

    Enviar exercício 1006
    1006 - Média 2

--- C#/1006/Program.cs
+++ C#/1006/Program.cs
@@ -4,6 +4,14 @@ public class Program
 {
     public static void Main()
     {
-        Console.WriteLine("Hello World!");        
+        double A, B, C, MEDIA;
+
+        A = double.Parse(Console.ReadLine());
+        B = double.Parse(Console.ReadLine());
+        C = double.Parse(Console.ReadLine());
+
+        MEDIA = (A * 2 + B * 3 + C * 5) / 10;
+
+        Console.WriteLine("MEDIA = " + MEDIA.ToString("F1"));
     }
 }
