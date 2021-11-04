61581e3a4c350375d04b82516abddfd20373d5db
Vitor Barbieri da Silva <vbarbierisilva@gmail.com>
2021-11-04 17:39:01 -0300

    Enviar exercício 1010
    1010 - Cálculo Simples

--- C#/1010/Program.cs
+++ C#/1010/Program.cs
@@ -4,6 +4,25 @@ public class Program
 {
     public static void Main()
     {
-        Console.WriteLine("Hello World!");        
+        int cd1, cd2, qt1, qt2;
+        double vl1, vl2, total;
+        string s1, s2;
+
+        s1 = Console.ReadLine();
+        s2 = Console.ReadLine();
+
+        string[] vet1 = s1.Split(' ');
+        string[] vet2 = s2.Split(' ');
+
+        cd1 = int.Parse(vet1[0]);
+        cd2 = int.Parse(vet2[0]);
+        qt1 = int.Parse(vet1[1]);
+        qt2 = int.Parse(vet2[1]);
+        vl1 = double.Parse(vet1[2]);
+        vl2 = double.Parse(vet2[2]);
+
+        total = qt1 * vl1 + qt2 * vl2;
+
+        Console.WriteLine("VALOR A PAGAR: R$ " + total.ToString("F2", CultureInfo.InvariantCulture));
     }
 }
