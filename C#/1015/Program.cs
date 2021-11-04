0f821d11de62ebbbb7d2cf27e90f708a93f4df06
Vitor Barbieri da Silva <vbarbierisilva@gmail.com>
2021-11-04 17:42:58 -0300

    Enviar exercício 1015
    1015 - Distância Entre Dois Pontos

--- C#/1015/Program.cs
+++ C#/1015/Program.cs
@@ -4,6 +4,21 @@ public class Program
 {
     public static void Main()
     {
-        Console.WriteLine("Hello World!");        
+        string str1, str2;
+        double x1, x2, y1, y2, dist;
+
+        str1 = Console.ReadLine();
+        string[] vet1 = str1.Split(' ');
+        str2 = Console.ReadLine();
+        string[] vet2 = str2.Split(' ');
+
+        x1 = double.Parse(vet1[0]);
+        y1 = double.Parse(vet1[1]);
+        x2 = double.Parse(vet2[0]);
+        y2 = double.Parse(vet2[1]);
+
+        dist = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+
+        Console.WriteLine(dist.ToString("F4"));
     }
 }
