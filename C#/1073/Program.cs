ff2dc0de4b7e6c72b2eb57fdf742cffee93160e5
Vitor Barbieri da Silva <vbarbierisilva@gmail.com>
2021-11-04 18:44:18 -0300

    Enviar exercício 1073
    1073 - Quadrado de Pares

--- C#/1073/Program.cs
+++ C#/1073/Program.cs
@@ -4,6 +4,14 @@ public class Program
 {
     public static void Main()
     {
-        Console.WriteLine("Hello World!");        
+        int num = int.Parse(Console.ReadLine());
+
+        if (num >= 5 && num <= 2000)
+        {
+            for (int i = 2; i <= num; i += 2)
+            {
+                Console.WriteLine(i + "^2 = " + Math.Pow(i, 2));
+            }
+        }
     }
 }
