200d061c8c1b8082834d03840ea45ab7d740beca
Vitor Barbieri da Silva <vbarbierisilva@gmail.com>
2021-11-04 17:41:04 -0300

    Enviar exercício 1011
    1011 - Esfera

--- C#/1011/Program.cs
+++ C#/1011/Program.cs
@@ -4,6 +4,12 @@ public class Program
 {
     public static void Main()
     {
-        Console.WriteLine("Hello World!");        
+        double raio, volume;
+
+        raio = double.Parse(Console.ReadLine());
+
+        volume = (4.0 / 3) * 3.14159 * Math.Pow(raio, 3);
+
+        Console.WriteLine("VOLUME = " + volume.ToString("F3"));       
     }
 }
