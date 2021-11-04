90290cfe8d3bfc36a845b6b3c3007ac86ab23911
Vitor Barbieri da Silva <vbarbierisilva@gmail.com>
2021-11-04 17:53:53 -0300

    Enviar exercício 1039
    1038 - Lanche

--- C#/1038/Program.cs
+++ C#/1038/Program.cs
@@ -4,6 +4,33 @@ public class Program
 {
     public static void Main()
     {
-        Console.WriteLine("Hello World!");        
+        string texto = Console.ReadLine();
+        string[] vet = texto.Split(' ');
+
+        int produto = int.Parse(vet[0]);
+        int quantidade = int.Parse(vet[1]);
+        double total = 0;
+
+        switch (produto)
+        {
+            case 1:
+                total = 4 * quantidade;
+                break;
+            case 2:
+                total = 4.5 * quantidade;
+                break;
+            case 3:
+                total = 5 * quantidade;
+                break;
+            case 4:
+                total = 2 * quantidade;
+                break;
+            case 5:
+                total = 1.5 * quantidade;
+                break;
+            default:
+                break;
+        }
+        Console.WriteLine("Total: R$ " + total.ToString("F2"));
     }
 }
