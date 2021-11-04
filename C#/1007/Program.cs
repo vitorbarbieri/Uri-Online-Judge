1d7ff58d5c13a3127287a1a4930f8a93f57c5d85
Vitor Barbieri da Silva <vbarbierisilva@gmail.com>
2021-11-04 17:36:43 -0300

    Enviar exercício 1007
    1007 - Diferença

--- C#/1007/Program.cs
+++ C#/1007/Program.cs
@@ -4,6 +4,15 @@ public class Program
 {
     public static void Main()
     {
-        Console.WriteLine("Hello World!");        
+        int A, B, C, D, DIFERENCA;
+
+        A = int.Parse(Console.ReadLine());
+        B = int.Parse(Console.ReadLine());
+        C = int.Parse(Console.ReadLine());
+        D = int.Parse(Console.ReadLine());
+
+        DIFERENCA = A * B - C * D;
+
+        Console.WriteLine("DIFERENCA = " + DIFERENCA);      
     }
 }
