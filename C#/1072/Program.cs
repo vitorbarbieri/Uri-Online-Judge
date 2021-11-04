a1c88ec4c720d2d7a8c036a327ccab4fd8701c3f
Vitor Barbieri da Silva <vbarbierisilva@gmail.com>
2021-11-04 18:39:07 -0300

    Enviar exercício 1072
    1072 - Intervalo 2

--- C#/1072/Program.cs
+++ C#/1072/Program.cs
@@ -4,6 +4,22 @@ public class Program
 {
     public static void Main()
     {
-        Console.WriteLine("Hello World!");        
+        int qtd = int.Parse(Console.ReadLine());
+        int valor, dentro = 0, fora = 0;
+
+        for (int i = 1; i <= qtd; i++)
+        {
+            valor = int.Parse(Console.ReadLine());
+            if (valor >= 10 && valor <= 20)
+            {
+                dentro += 1;
+            }
+            else
+            {
+                fora += 1;
+            }
+        }
+        Console.WriteLine(dentro + " in");
+        Console.WriteLine(fora + " out");
     }
 }
