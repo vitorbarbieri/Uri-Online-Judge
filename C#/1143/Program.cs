b7ac6d395a6c826982623ae07801e21886f43e4b
Vitor Barbieri da Silva <vbarbierisilva@gmail.com>
2021-11-05 16:39:45 -0300

    Enviar exercício 1143
    1143 - Quadrado e ao Cubo

--- C#/1143/Program.cs
+++ C#/1143/Program.cs
@@ -4,6 +4,11 @@ public class Program
 {
     public static void Main()
     {
-        Console.WriteLine("Hello World!");        
+        int n1 = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= n1; i++)
+        {
+            Console.WriteLine(i + " " + (Math.Pow(i,2)) + " " + (Math.Pow(i,3)));
+        }    
     }
 }
