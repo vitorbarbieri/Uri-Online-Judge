5fd63ef3f1b2befa5ebae130f97c6f3b990b239a
Vitor Barbieri da Silva <vbarbierisilva@gmail.com>
2021-11-05 17:49:58 -0300

    Enviar exercício 1080
    1080 - Maior e Posição

--- C#/1080/Program.cs
+++ C#/1080/Program.cs
@@ -4,6 +4,18 @@ public class Program
 {
     public static void Main()
     {
-        Console.WriteLine("Hello World!");        
+        int maior = 0, posicao = 0;
+
+        for (int i = 1; i <= 100; i++)
+        {
+            int valor = int.Parse(Console.ReadLine());
+            if (valor > maior)
+            {
+                maior = valor;
+                posicao = i;
+            }
+        }
+        Console.WriteLine(maior);
+        Console.WriteLine(posicao);
     }
 }
