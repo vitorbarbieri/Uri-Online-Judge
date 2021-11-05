eef341b8b705b2111a4c22ac563ee1d1aacb2b96
Vitor Barbieri da Silva <vbarbierisilva@gmail.com>
2021-11-05 15:35:46 -0300

    Enviar exercício 1151
    1151 - Fibonacci Fácil

--- C#/1151/Program.cs
+++ C#/1151/Program.cs
@@ -4,6 +4,24 @@ public class Program
 {
     public static void Main()
     {
-        Console.WriteLine("Hello World!");        
+        int n = int.Parse(Console.ReadLine());
+        int anterior1, anterior2;
+        anterior1 = 0;
+        anterior2 = 1;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (i == n - 1)
+            {
+                Console.WriteLine(anterior1);
+            }
+            else
+            {
+                Console.Write(anterior1 + " ");
+            }
+            int novo = anterior1 + anterior2;
+            anterior1 = anterior2;
+            anterior2 = novo;
+        }
     }
 }
