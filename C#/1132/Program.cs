1a61317c50fcf5598b2c0876d840e5ea5138dd6c
Vitor Barbieri da Silva <vbarbierisilva@gmail.com>
2021-11-05 16:50:57 -0300

    Enviar exercício 1132
    1132 - Múltiplos de 13

--- C#/1132/Program.cs
+++ C#/1132/Program.cs
@@ -4,6 +4,28 @@ public class Program
 {
     public static void Main()
     {
-        Console.WriteLine("Hello World!");        
+        int n1 = int.Parse(Console.ReadLine());
+        int n2 = int.Parse(Console.ReadLine());
+        int menor, maior, soma = 0;
+
+        if (n1 < n2)
+        {
+            menor = n1;
+            maior = n2;
+        }
+        else
+        {
+            menor = n2;
+            maior = n1;
+        }
+
+        for (int i = menor; i <= maior; i++)
+        {
+            if (i % 13 != 0)
+            {
+                soma += i;
+            }
+        }
+        Console.WriteLine(soma);
     }
 }
