47237acaa5d26587a7cec83006b692ea55f9ad3d
Vitor Barbieri da Silva <vbarbierisilva@gmail.com>
2021-11-05 16:56:05 -0300

    Enviar exercício 1116
    1116 - Dividindo X por Y

--- C#/1116/Program.cs
+++ C#/1116/Program.cs
@@ -4,6 +4,22 @@ public class Program
 {
     public static void Main()
     {
-        Console.WriteLine("Hello World!");        
+        int qtd = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < qtd; i++)
+        {
+            string[] valores = Console.ReadLine().Split(' ');
+            int n1 = int.Parse(valores[0]);
+            int n2 = int.Parse(valores[1]);
+            if (n2 == 0)
+            {
+                Console.WriteLine("divisao impossivel");
+            }
+            else
+            {
+                double divisao = (double)n1 / n2;
+                Console.WriteLine(divisao.ToString("F1"));
+            }
+        }
     }
 }
