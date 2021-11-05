2b98ce6910b3a7fda95feb4a0b1aa180bf5985fb
Vitor Barbieri da Silva <vbarbierisilva@gmail.com>
2021-11-05 17:40:47 -0300

    Enviar exercício 1075
    1075 - Resto 2

--- C#/1075/Program.cs
+++ C#/1075/Program.cs
@@ -4,6 +4,14 @@ public class Program
 {
     public static void Main()
     {
-        Console.WriteLine("Hello World!");        
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 2; i < 10000; i++)
+        {
+            if (i % n == 2)
+            {
+                Console.WriteLine(i);
+            }
+        }
     }
 }
