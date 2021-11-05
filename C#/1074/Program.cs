fb28a7c0d3331b67fcfbcf31a3a9d6b6b0e36fe2
Vitor Barbieri da Silva <vbarbierisilva@gmail.com>
2021-11-05 17:37:05 -0300

    Enviar exercício 1074
    1074 - Par ou Ímpar

--- C#/1074/Program.cs
+++ C#/1074/Program.cs
@@ -4,6 +4,35 @@ public class Program
 {
     public static void Main()
     {
-        Console.WriteLine("Hello World!");        
+        int qtd = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= qtd; i++)
+        {
+            int n = int.Parse(Console.ReadLine());
+            if (n == 0)
+            {
+                Console.WriteLine("NULL");
+            }
+            else
+            {
+                if (n % 2 == 0)
+                {
+                    Console.Write("EVEN ");
+                }
+                else
+                {
+                    Console.Write("ODD ");
+                }
+
+                if (n < 0)
+                {
+                    Console.WriteLine("NEGATIVE");
+                }
+                else
+                {
+                    Console.WriteLine("POSITIVE");
+                }
+            }
+        }
     }
 }
