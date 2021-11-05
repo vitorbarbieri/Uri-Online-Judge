d527ab9f90494c04d957d3108e93c7bd1c99374f
Vitor Barbieri da Silva <vbarbierisilva@gmail.com>
2021-11-05 19:02:49 -0300

    Enviar exercício 1098
    1098 - Sequencia IJ 4

--- C#/1098/Program.cs
+++ C#/1098/Program.cs
@@ -4,6 +4,25 @@ public class Program
 {
     public static void Main()
     {
-        Console.WriteLine("Hello World!");        
+        double i, j;
+        int a, b;
+
+        for (i = 0; i < 2.1; i += 0.2)
+        {
+            for (j = 1; j <= 3; j++)
+            {
+                i = Math.Round(i, 2);
+                if (i > 0 && i < 1 || i > 1 && i < 2)
+                {
+                    Console.WriteLine("I=" + i.ToString("F1") + " J=" + (i + j).ToString("F1"));
+                }
+                else
+                {
+                    a = (int)i;
+                    b = (int)(i + j);
+                    Console.WriteLine("I=" + a + " J=" + b);
+                }
+            }
+        }
     }
 }
