d9c81b5bd5f63e32668e40646cdd5f1b6e0f3828
Vitor Barbieri da Silva <vbarbierisilva@gmail.com>
2021-11-05 16:48:37 -0300

    Enviar exercício 1133
    1133 - Resto da Divisão

--- C#/1133/Program.cs
+++ C#/1133/Program.cs
@@ -4,6 +4,27 @@ public class Program
 {
     public static void Main()
     {
-        Console.WriteLine("Hello World!");        
+        int n1 = int.Parse(Console.ReadLine());
+        int n2 = int.Parse(Console.ReadLine());
+        int menor, maior;
+
+        if (n1 < n2)
+        {
+            menor = n1;
+            maior = n2;
+        }
+        else
+        {
+            menor = n2;
+            maior = n1;
+        }
+
+        for (int i = menor + 1; i < maior; i++)
+        {
+            if (i % 5 == 2 || i % 5 == 3)
+            {
+                Console.WriteLine(i);
+            }
+        }
     }
 }
