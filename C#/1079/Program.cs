fc9974c9ccf27d92815ca5efb5fe85ade403a0f6
Vitor Barbieri da Silva <vbarbierisilva@gmail.com>
2021-11-05 17:46:32 -0300

    Enviar exercício 1079
    1079 - Médias Ponderadas

--- C#/1079/Program.cs
+++ C#/1079/Program.cs
@@ -4,6 +4,16 @@ public class Program
 {
     public static void Main()
     {
-        Console.WriteLine("Hello World!");        
+        int qtd = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= qtd; i++)
+        {
+            string[] notas = Console.ReadLine().Split(' ');
+            double n1 = double.Parse(notas[0]);
+            double n2 = double.Parse(notas[1]);
+            double n3 = double.Parse(notas[2]);
+            double media = (n1 * 2 + n2 * 3 + n3 * 5) / 10;
+            Console.WriteLine(media.ToString("F1"));
+        }
     }
 }
