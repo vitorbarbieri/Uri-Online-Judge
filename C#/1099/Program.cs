8f8882e9894251c518fe506d21b40363b063611c
Vitor Barbieri da Silva <vbarbierisilva@gmail.com>
2021-11-05 18:33:29 -0300

    Enviar exercício 1099
    1099 - Soma de Ímpares Consecutivos II

--- C#/1099/Program.cs
+++ C#/1099/Program.cs
@@ -4,6 +4,34 @@ public class Program
 {
     public static void Main()
     {
-        Console.WriteLine("Hello World!");        
+        int quantidade = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= quantidade; i++)
+        {
+            string[] valores = Console.ReadLine().Split(' ');
+            int n1 = int.Parse(valores[0]);
+            int n2 = int.Parse(valores[1]);
+            int menor, maior, soma = 0;
+
+            if (n1 < n2)
+            {
+                menor = n1;
+                maior = n2;
+            }
+            else
+            {
+                menor = n2;
+                maior = n1;
+            }
+
+            for (int j = menor + 1; j < maior; j++)
+            {
+                if (j % 2 != 0)
+                {
+                    soma += j;
+                }
+            }
+            Console.WriteLine(soma);
+        }
     }
 }
