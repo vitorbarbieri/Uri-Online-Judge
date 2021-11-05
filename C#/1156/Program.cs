2ba6eaf16a37a8c94aca6f6d1cd24303dbae6d63
Vitor Barbieri da Silva <vbarbierisilva@gmail.com>
2021-11-05 14:59:58 -0300

    Enviar exercício 1156
    1156 - Sequência S II

--- C#/1156/Program.cs
+++ C#/1156/Program.cs
@@ -4,6 +4,16 @@ public class Program
 {
     public static void Main()
     {
-        Console.WriteLine("Hello World!");        
+        double s = 1, divisao;
+        int dividendo = 3, divisor = 2;
+
+        while (dividendo <= 39)
+        {
+            divisao = (double)dividendo / divisor;
+            s += divisao;
+            dividendo += 2;
+            divisor *= 2;
+        }
+        Console.WriteLine(s.ToString("F2"));
     }
 }
