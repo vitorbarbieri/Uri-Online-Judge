b668a16c6926a0d8fb7bee2702380a8e1b044e7b
Vitor Barbieri da Silva <vbarbierisilva@gmail.com>
2021-11-05 16:34:14 -0300

    Enviar exercício 1144
    1144 - Sequência Lógica

--- C#/1144/Program.cs
+++ C#/1144/Program.cs
@@ -4,6 +4,14 @@ public class Program
 {
     public static void Main()
     {
-        Console.WriteLine("Hello World!");        
+        int n1 = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= n1; i++)
+        {
+            int n2 = i * i;
+            int n3 = i * i * i;
+            Console.WriteLine(i + " " + n2 + " " + n3);
+            Console.WriteLine(i + " " + (n2 + 1) + " " + (n3 + 1));
+        }
     }
 }
